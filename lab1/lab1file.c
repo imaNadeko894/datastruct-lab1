e5ff514f38f70055bb73f1c1538e61fc313666a9
imaNadeko894 <talaxath@gmail.com>
2026-07-15 17:28:19 +0800

    Add files via upload

---
+++ lab1/lab1file.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+FILE *fpr, *fpw;
+
+typedef struct Node *PtrNode;
+struct Node {
+    int num;
+    int data;
+    PtrNode next;
+};
+
+PtrNode BuildList() {
+    int n;
+    fscanf(fpr, "%d", &n);
+    if (n < 1) return NULL;
+
+    PtrNode head = (PtrNode)malloc(sizeof(struct Node));
+    PtrNode current = head;
+    head->num = 1;
+    fscanf(fpr, "%d", &head->data);
+
+    for (int i = 2; i <= n; i++) {
+        PtrNode newNode = (PtrNode)malloc(sizeof(struct Node));
+        newNode->num = i;
+        fscanf(fpr, "%d", &newNode->data);
+        current->next = newNode;
+        current = newNode;
+    }
+    current->next = head; // Make the list circular
+    return current; // Return the last node to facilitate circular traversal
+}
+
+PtrNode CountOff(PtrNode head, int *m) {
+    if (*m < 1) return head; // Invalid count-off number
+    if (head->next == head) {
+        fprintf(fpw, "%d\n", head->num);
+        free(head);
+        return NULL; // Only one node left
+    }
+
+    PtrNode current = head;
+    for (int i = 1; i < *m; i++) 
+        current = current->next;
+
+    PtrNode toDelete = current->next;
+    fprintf(fpw, "%d ", toDelete->num);
+    current->next = toDelete->next; // Remove the node
+    *m = toDelete->data;
+    free(toDelete);
+    return current; 
+}
+
+int main() {
+    fpr = fopen("lab1in.txt", "r");
+    fpw = fopen("lab1out.txt", "w");
+
+    PtrNode head = BuildList();
+    int m;
+    fscanf(fpr,"%d", &m);
+
+    while (head != NULL) {
+        head = CountOff(head, &m);
+    }
+
+    fclose(fpr);
+    fclose(fpw);
+    return 0;
+}
